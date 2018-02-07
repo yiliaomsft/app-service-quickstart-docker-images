1d5654daf148c1a3be5692425ddd3736e21a71f8
leonzhang77 <zhangjianfeng73@hotmail.com>
2018-02-07 06:11:01 +0000

    Use docker inspect to check image

--- travis-script/test-dockerfile.sh
+++ travis-script/test-dockerfile.sh
@@ -8,7 +8,12 @@ function _do()
 }
 
 test_Dockerfile(){
-    testSSH=$(cat Dockerfile | grep EXPOSE | grep 2222)
+    _do docker inspect ${DOCKER_IMAGE_NAME} > inspect.json
+    echo "INFORMATION: Details of "${DOCKER_IMAGE_NAME}":"
+    cat inspect.json
+
+    # Check Ports
+    testSSH=$(jq .[].Config.ExposedPorts inspect.json | grep 2222)
     if [ -z "${testSSH}" ]; then 
         echo "FAILED - PORT 2222 isn't opened, SSH isn't working!!!"
         exit 1
@@ -16,23 +21,22 @@ test_Dockerfile(){
         echo "${testSSH}"
         echo "PASSED - PROT 2222 is opened."
     fi
-
-    testVOLUME=$(cat Dockerfile | grep VOLUME)
+    
+    # Check Volume
+    testVOLUME=$(jq .[].Config.Volumes inspect.json | grep null)
     if [ -z "${testVOLUME}" ]; then 
-        echo "PASSED - Great, there is no VOLUME lines."    
-    else
+        testVOLUME=$(jq .[].Config.Volumes inspect.json)
         echo "${testVOLUME}"
         echo "FAILED - These VOLUME lines should not be existed!!!"
         exit 1
+    else
+        echo "PASSED - Great, there is no VOLUME lines."            
     fi
 }
 
 echo "Docker: "${DOCKER_IMAGE_NAME}"/"${DOCKER_IMAGE_VERSION}
 echo "Docker: "${DOCKER_IMAGE_NAME}"/"${DOCKER_IMAGE_VERSION} >> result.log
-_do cd ${DOCKER_IMAGE_NAME}"/"${DOCKER_IMAGE_VERSION}
 test_Dockerfile
-_do cd ..
-_do cd ..
 echo "PASSED - Passed this stage." >> result.log
 
 
