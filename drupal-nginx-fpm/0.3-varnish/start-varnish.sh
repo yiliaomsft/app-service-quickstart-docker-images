3ac0ea7a1acfcad34d3e9891ac1f1f78b93665e4
LeonZhang77 <zhangjianfeng73@hotmail.com>
2018-05-02 14:35:27 +0800

    Revert "Created drupal-nginx-fpm:0.3-varnish (#39)" (#40)
    This reverts commit ebc267eddba1d96c81c1d93c90be52433c057645.

--- drupal-nginx-fpm/0.3-varnish/start-varnish.sh
+++
@@ -1,9 +0,0 @@
-RESPONSE_CODE=""
-printf 'Waiting for Nginx\n'
-while [ -z "$RESPONSE_CODE" ]
-do
-    RESPONSE_CODE=$(curl -s -I http://localhost:8080 | grep HTTP | awk {'print $2'})
-    sleep 2
-done
-printf 'Nginx ready. Starting Varnish\n'
-/usr/sbin/varnishd -s malloc,${VARNISH_CACHE_SIZE} -a ${HOST_IP}:${VARNISH_PORT} -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT}
